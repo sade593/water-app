255c6fbcb1d9c958da9e7016cddf93726ee6d0f8
unknown <guwsc@DESKTOP-M8R66DA.water.local>
2025-11-14 15:58:03 +0400

    vercel

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EmployeesView from '../views/EmployeesView.vue'
-import AddEmployeeView from '../views/AddEmployeeView.vue'
+import AddEmployeeView from '../views/AddEmployeeview.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
