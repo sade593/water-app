167082e30c6a72baffdc99ce3573aab4c30faae6
unknown <guwsc@DESKTOP-M8R66DA.water.local>
2025-11-14 16:13:51 +0400

    Add correct file casing

---
+++ src/views/AddEmployeeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-white p-6 rounded-lg shadow-md">
+    <h1 class="text-2xl font-bold mb-4">დაამატე ახალი თანამშრომელი</h1>
+    <p>Employee creation form will be here.</p>
+  </div>
+</template>
